44ccef6b290e865412b11b5ca83b444549b30843
gamersa22 <140577703+gamersa22@users.noreply.github.com>
2026-04-06 09:09:45 +0930

    2.00.1
    removing code snippet i left behind

--- MapPins.lua
+++ MapPins.lua
@@ -1707,15 +1707,7 @@ local function OnLootReceived(_, receivedBy, itemName, quantity, itemSound, loot
 		if COMPASS_PINS then COMPASS_PINS:RefreshPins(CustomPins[69].name) end
 	end
 end
-SLASH_COMMANDS["/data"]=function()
-	SavedVars.MapIdMap ={}
-	for i=1,10000,1 do
-		local mapTexure = GetMapTileTextureForMapId(i)
-		if mapTexure ~= "" then		
-			SavedVars.MapIdMap[mapTexure:match("[^\\/]+$"):lower():gsub("%.dds$", ""):gsub("_[0-9]+$", "")] = i
-		end
-	end
-end
+
 local function OnAchievementUpdate(achievementId,link)
 	LastAchivement=achievementId
 --[[
