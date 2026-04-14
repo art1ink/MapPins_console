259b213180c8e0f7a81d966c58c380c7b5d3d564
gamersa22 <140577703+gamersa22@users.noreply.github.com>
2026-04-14 11:59:52 +0930

    Script Fix
    added in Support for fishing node

--- PC MapPIns Data Extract/Script For MapIdMap List.lua
+++ PC MapPIns Data Extract/Script For MapIdMap List.lua
@@ -2,8 +2,11 @@ SLASH_COMMANDS["/data"]=function()
 	SavedVars.MapIdMap ={}
 	for i=1,10000,1 do
 		local mapTexure = GetMapTileTextureForMapId(i)
-		if mapTexure ~= "" then		
+		if mapTexure ~= "" then	
 			SavedVars.MapIdMap[mapTexure:match("[^\\/]+$"):lower():gsub("%.dds$", ""):gsub("_[0-9]+$", "")] = i
 		end
 	end
+	SavedVars.MapIdMap["u48_overland_base_west"] = 2603.1,
+	SavedVars.MapIdMap["u48_overland_base_east"] = 2603.2,
+	d("Extract Done")
 end
